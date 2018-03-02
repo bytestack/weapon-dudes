8fb73e4b21b88d609c11ff4d93759e8cd2d5713a
Marco Facetti <marco.facetti@trviadis.com>
2018-03-02 18:25:08 +0100

    Add spring boot deps, and add controller

---
+++ src/main/java/com/trivadis/blockchain/rest/WeaponTransactionController.java
@@ -0,0 +1,29 @@
+package com.trivadis.blockchain.rest;
+/*
+ * (C) Copyright 2015-2017 Trivadis AG. All rights reserved.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author Marco Facetti
+ */
+@Controller
+public class WeaponTransactionController {
+
+    @GetMapping("/transactions")
+    public String getAllWeaponTransactions() {
+
+        return "test";
+    }
+
+    @PostMapping
+    public String putWeaponTransaction() {
+
+        return "redirect:/transactions";
+    }
+
+
+}
