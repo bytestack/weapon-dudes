b036c84decd7374bf2281f55066ee3d0216299a1
Marius Quadflieg <mithandir@gmail.com>
2018-03-03 11:40:47 +0100

    Fix Test

--- src/test/java/com/trivadis/blockchain/rest/WeaponTransactionControllerTest.java
+++ src/test/java/com/trivadis/blockchain/rest/WeaponTransactionControllerTest.java
@@ -1,18 +1,12 @@
 package com.trivadis.blockchain.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.trivadis.blockchain.model.Person;
-import com.trivadis.blockchain.model.Weapon;
-import com.trivadis.blockchain.model.WeaponTransaction;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.time.OffsetDateTime;
-import java.util.Collections;
 
 import static org.junit.Assert.assertThat;
 
@@ -26,25 +20,15 @@ public class WeaponTransactionControllerTest {
     @Test
     public void transaktion_putWeaponTransaction_ok() throws JsonProcessingException {
         //given
-        Person seller = new Person();
-        seller.setWeaponRegisterNumber("123");
-
-        Person buyer = new Person();
-        buyer.setWeaponRegisterNumber("234");
-
-        WeaponTransaction weaponTransaction = new WeaponTransaction();
-        weaponTransaction.setTimestamp(OffsetDateTime.now());
-
-        Weapon weapon = new Weapon();
-        weapon.setSerialNumber("999");
-        weapon.setTransactions(Collections.singletonList(weaponTransaction));
+        WeaponTransactionDto dto = new WeaponTransactionDto();
+        dto.setBuyerWeaponRegisterNumber("123");
+        dto.setSellerWeaponRegisterNumber("234");
+        dto.setWeaponSerialNumber("999");
 
         //when
-        final String result = testee.putWeaponTransaction(seller, buyer, weapon);
+        final String result = testee.putWeaponTransaction(dto);
 
         //then
         assertThat(result, Matchers.equalTo("redirect:/transactions"));
     }
-
-
 }
